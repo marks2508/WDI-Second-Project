fae19abb9d240926b2862c1d1b2c5ef204fac4a4
Mark S <markscicluna@hotmail.com>
2018-01-15 18:43:49 +0000

    gifts now showing in list - need to create link to individual gift and delete/edit gift

--- db/seeds.js
+++ db/seeds.js
@@ -3,34 +3,3 @@ mongoose.Promise = require('bluebird');
 
 const { dbURI } = require('../config/environment');
 mongoose.connect(dbURI);
-
-
-const List = require('../models/list');
-const User = require('../models/user');
-const Gift = require('../models/list');
-
-List.collection.drop();
-User.collection.drop();
-
-List
-  .create([{
-    name: 'Christmas'
-  },{
-    name: 'Birthdays'
-  },{
-    name: 'Anniverseries'
-  }])
-  .then((lists) => {
-    console.log(`${lists.length} lists created`);
-    return Gift
-      .create([{
-        name: 'Book',
-        age: '35',
-        interests: 'Art',
-        budget: '99',
-        ideas: 'Books'
-      }]);
-  })
-  .then((gifts) => console.log(`${gifts.length} gifts created`))
-  .catch((err) => console.log(err))
-  .finally(() => mongoose.connection.close());
